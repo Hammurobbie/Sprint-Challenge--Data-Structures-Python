a1067b0c6f5495b89c33c5848bb0a6ada12def01
Robbie Porter <rthomporter@gmail.com>
2020-04-24 11:46:27 -0500

    names

--- names/names.py
+++ names/names.py
@@ -12,15 +12,21 @@ f.close()
 
 duplicates = []  # Return the list of duplicates in this data structure
 
+# OG time complexity O(n^2)
+
 # Replace the nested for loops below with your improvements
 for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+    if name_1 in names_2:
+        duplicates.append(name_1)
+
 
 end_time = time.time()
-print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
-print (f"runtime: {end_time - start_time} seconds")
+
+print(len(duplicates))
+print(duplicates)
+print(end_time - start_time)
+# print (f'{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n')
+# print (f'runtime: {end_time - start_time} seconds')
 
 # ---------- Stretch Goal -----------
 # Python has built-in tools that allow for a very efficient approach to this problem
